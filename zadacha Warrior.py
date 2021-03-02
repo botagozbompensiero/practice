bd32b660887099659b3888f68103149df9f0f384
botagozbompensiero <78722298+botagozbompensiero@users.noreply.github.com>
2021-03-02 12:39:33 +0200

    Update zadacha Warrior.py

--- zadacha Warrior.py
+++ zadacha Warrior.py
@@ -12,17 +12,24 @@ class Warrior:
         self.health = 100
 
     def battle(self, other):
-        while self.health > 0:
+        if self.health > 0:
             self.health = self.health - 20
             print(f'{self.name}"s health is {self.health}. Napadal {other.name}')
-        print(f'{other.name} is the winner')
-
-
+        if self.health == 0:
+            print(f'{other.name} is the winner')
+        if other.health == 0:
+            print(f'{self.name} is the winner')
 
 
 Warrior_1 = Warrior()
 Warrior_2 = Warrior()
 Warrior_1.setName('Judy')
 Warrior_2.setName('Mila')
-Warrior.battle(Warrior_1, Warrior_2)
+
+while Warrior_1.health != 0 and Warrior_2.health != 0:
+    rand = random.randint(1, 3)
+    if rand == 1:
+        Warrior.battle(Warrior_1, Warrior_2)
+    if rand == 2:
+        Warrior.battle(Warrior_2, Warrior_1)
 
